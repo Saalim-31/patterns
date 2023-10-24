9d58ee0202f658ccd94f24d507562e9b9aadf8d9
Saalim-31 <134718581+Saalim-31@users.noreply.github.com>
2023-10-24 16:29:26 +0100

    Add files via upload

---
+++ starpattern4.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int rows,j, i;
+    printf("Enter number of rows ");
+    scanf("%d",&rows);
+    for(i=1;i<=rows;i++)
+    {
+        for(j=rows;j>=i;j--)
+
+
+
+
+        printf("*");
+
+            printf("\n");}
+
+}
+
+
